85f92af306518acb2d5f5c326c1550fc9e5c755e
vadik <vadik94_1rec@bk.ru>
2021-04-02 19:38:24 +0500

    Выполненное задание урока №2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-const money = 100;
-const income = 100;
-const addExpenses = 100;
-const deposi = 100;
-const mission = 100;
-const period = 100;
+const money = 100000;
+const income = 'Вклад в банке';
+const addExpenses = 'Еда, Зал, Интернет, Заправка';
+const deposit = true;
+const mission = 123456789;
+const period = 8;
+let budgetDay;
 
-alert('lesson01');
-console.log('lesson01');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period +  ' месяцев');
+console.log('Цель заработать ' + mission +  ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay = money/30);
